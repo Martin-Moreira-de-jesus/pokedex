743179522148e76893cb7a41d9c94d28a140c5d8
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2022-11-01 15:29:26 +0100

    refactor!: switch to ES7 async/await, removed sccs
    Easier to read and avoids promise chaining. No more scss loader, couldn't fix the errors that came with it.
    
    BREAKING CHANGE : import processed css file instead of scss file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "@/assets/custom-bootstrap.scss";
+import "@/assets/custom.css";
 import "bootstrap/dist/js/bootstrap.js";
 
 createApp(App).mount("#app");
--- src/services/api.js
+++ src/services/api.js
@@ -4,28 +4,31 @@ const pokedex = new Pokedex();
 
 export default {
   getPokemons: async (offset, limit) => {
-    return pokedex
-      .getPokemonsList({ offset: offset, limit: limit })
-      .then((response) => {
-        const promises = response.results.map((element) =>
-          pokedex.getPokemonByName(element.name)
-        );
-
-        return Promise.all(promises);
-      })
-      .then((data) => {
-        return data.map((element) => {
-          return {
-            id: parseInt(element.id),
-            name: element.name,
-            sprite: element.sprites.other["official-artwork"].front_default,
-            types: element.types,
-          };
-        });
-      })
-      .catch((err) => {
-        alert("Something went wrong");
-        console.log(err);
+    try {
+      const response = await pokedex.getPokemonsList({
+        offset: offset,
+        limit: limit,
       });
+
+      const promises = response.results.map((element) =>
+        pokedex.getPokemonByName(element.name)
+      );
+
+      const data = await Promise.all(promises);
+
+      const pokemons = data.map((element) => {
+        return {
+          id: parseInt(element.id),
+          name: element.name,
+          sprite: element.sprites.other["official-artwork"].front_default,
+          types: element.types,
+        };
+      });
+
+      return pokemons;
+    } catch (err) {
+      alert("Something went wrong");
+      console.log(err);
+    }
   },
 };
