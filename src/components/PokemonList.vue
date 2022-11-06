8c1f6314c3d04daf6c2199ca61077792fe75cc41
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2022-11-06 15:45:22 +0100

    feat(pokemon-list): improve informations displayed on pokemon list
    Moved button, add message when no data was found on search.

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -3,17 +3,29 @@
     <div class="bg-white container flex-fill d-flex flex-row flex-wrap justify-content-evenly">
       <pokemon-card v-for="pokemon in pokemons" :id="pokemon.id" :key="pokemon.id" :name="pokemon.name"
                     :sprite="pokemon.sprite" :types="pokemon.types"/>
-      <p v-if="strategy !== 'list' && pokemons.length === 0" class="mt-2">
-        Oh oh... looks like what you're searching for doesn't exist
-      </p>
-      <button v-if="!isFull" class="btn btn-primary text-white" @click="getPokemons">LOAD MORE</button>
+      <div v-if="strategy !== 'list' && pokemons.length === 0 && !isLoading" class="mt-2 p-2 not-found">
+        <p>
+          Oh oh...<br>
+          Looks no Pokémon matched your search!
+        </p>
+        <img :src="require('@/assets/images/sad-pikachu.webp')" alt="no pokemon found image" style="width: 200px; height:200px;">
+        <p>You could try being less specific</p>
+      </div>
     </div>
+    <Vue3Lottie :animationData="Loader"
+                :height="50"
+                :width="50"
+                v-if="isLoading" />
+    <button v-if="!isFull && !isLoading" class="btn btn-primary text-white" @click="getPokemons">LOAD MORE</button>
   </section>
 </template>
 
 <script>
 import PokemonCard from "@/components/PokemonCard";
 import API from "@/services/api";
+import {Vue3Lottie} from 'vue3-lottie';
+import Loader from '@/assets/lotties/loader.json'
+import 'vue3-lottie/dist/style.css'
 
 export default {
   data() {
@@ -25,12 +37,15 @@ export default {
       limit: 20,
       /** @var isFull true when all possibilites have been displayed used to make load more button disappear **/
       isFull: false,
+      isLoading: false,
       pokemons: [],
+      Loader,
     }
   },
   methods: {
     async getPokemons() {
       try {
+        this.isLoading = true;
         let result;
         switch (this.strategy) {
           case 'search':
@@ -47,6 +62,7 @@ export default {
         this.pokemons = this.pokemons.concat(result.pokemons);
         this.isFull = result.isLast;
         this.offset += this.limit;
+        this.isLoading = false;
       } catch (err) {
         alert("Something went wrong, refresh the page and try again");
         this.pokemons = [];
@@ -80,10 +96,14 @@ export default {
   name: 'App',
   components: {
     PokemonCard,
+    Vue3Lottie,
   },
 }
 </script>
 
 <style scoped>
-
+.not-found {
+  border: red 2px solid;
+  border-radius: 5px;
+}
 </style>
