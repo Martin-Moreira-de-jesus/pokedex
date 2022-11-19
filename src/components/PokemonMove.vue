427956520dea2d5a0f44cdd4b812fd9ccc55bdd6
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2022-11-19 19:15:24 +0100

    feat: components to list a pokemon moves
    List pokemon moves based on a list of its moves

---
+++ src/components/PokemonMove.vue
@@ -0,0 +1,54 @@
+<template>
+  <tr>
+    <td></td>
+    <td class="text-capitalize text-start align-middle">{{ move.name.replace("-", " ") }}</td>
+    <td class="align-middle"><pokemon-small-chip v-if="moveDetail.type.name" :type="moveDetail.type.name"/></td>
+    <td class="align-middle"><pokemon-small-chip v-if="moveDetail.type.name" :type="moveDetail.damage_class.name"/></td>
+    <td class="align-middle">{{ moveDetail.power ?? '-' }}</td>
+    <td class="align-middle">{{ moveDetail.pp ?? '-' }}</td>
+    <td class="align-middle">{{ moveDetail.accuracy ?? '-' }}</td>
+    <td class="align-middle">{{ moveDetail.priority ?? '-' }}</td>
+    <td class="text-uppercase align-middle">{{ moveDetail.generation.name?.replace('generation-', '') }}</td>
+  </tr>
+</template>
+
+<script>
+import {pokedex} from '@/services/api';
+import PokemonSmallChip from "@/components/PokemonSmallChip";
+
+export default {
+  name: "PokemonMove",
+  components: {PokemonSmallChip},
+  props: {
+    move: {
+      type: Object,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      moveDetail: {
+        accuracy: '',
+        priority: '',
+        generation: {},
+        damage_class: {},
+        type: {},
+        pp: '',
+        power: '',
+      }
+    }
+  },
+  methods: {
+    async initMove() {
+      this.moveDetail = await pokedex.getMove(this.move.name);
+    }
+  },
+  mounted() {
+    this.initMove();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PokemonMovesDetail.vue
@@ -0,0 +1,52 @@
+<template>
+  <article class="bg-light">
+    <h4 class="pt-2">Moves</h4>
+    <div class="table-responsive ps-2 pe-2">
+      <table class="table">
+        <thead :class="`chip-${pokemonColor}`">
+        <tr>
+          <th>Level</th>
+          <th>Move</th>
+          <th>Type</th>
+          <th>Category</th>
+          <th>Power</th>
+          <th>PP</th>
+          <th>Accuracy</th>
+          <th>Priority</th>
+          <th>Generation</th>
+        </tr>
+        </thead>
+        <tbody>
+        <pokemon-move v-for="(move, index) in moves" :key="index" :move="move.move"/>
+        <tr :class="`chip-${pokemonColor}`">
+          <td colspan="9"></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </article>
+</template>
+
+<script>
+import PokemonMove from "@/components/PokemonMove";
+export default {
+  name: "PokemonMovesDetail",
+  components: {PokemonMove},
+  props: {
+    moves: {
+      type: Array,
+      required: true,
+    },
+    pokemonColor: {
+      type: String,
+      required: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+table {
+  font-size: 12px;
+}
+</style>
