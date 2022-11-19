0bac7daba15d711f0754fdac6e6278438c48212c
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2022-11-19 19:18:20 +0100

    feat: large chip component

---
+++ src/components/PokemonLargeChip.vue
@@ -0,0 +1,37 @@
+<template>
+  <p :class="typeClass(className)" class="btn-type p-0 m-0">
+    <span>{{ content ?? className }}</span>
+  </p>
+</template>
+
+<script>
+export default {
+  name: "PokemonLargeChip",
+  props: {
+    className: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: false
+    }
+  },
+  methods: {
+    typeClass(type) {
+      return `chip-${type}`;
+    },
+  }
+}
+</script>
+
+<style scoped>
+p {
+  width: 110px;
+}
+span {
+  position: relative;
+  top: 4px;
+  font-size: 13px;
+}
+</style>
