ca1f06c62c552f7184d18afa0c9f66a322d40570
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2022-11-19 19:12:28 +0100

    feat: add small chips for types

---
+++ src/components/PokemonSmallChip.vue
@@ -0,0 +1,30 @@
+<template>
+  <img :src="imagePath(type)"
+       :alt="`no damage from ${type}`"
+       class="type-chip"
+       :class="`chip-${type} type-chip-${type}`">
+</template>
+
+<script>
+export default {
+  name: "PokemonSmallChip",
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+  },
+  methods: {
+    imagePath(typeName) {
+      return require(`@/assets/images/types/${typeName}.png`)
+    },
+  }
+}
+</script>
+
+<style scoped>
+img {
+  height: 2.5rem;
+  width: 2.5rem;
+}
+</style>
