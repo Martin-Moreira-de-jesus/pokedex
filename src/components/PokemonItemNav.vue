9d1a612b191008867b737b37fe821db699bca708
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2022-11-20 16:03:44 +0100

    feat: add home button to pokemon item nav bar

--- src/components/PokemonItemNav.vue
+++ src/components/PokemonItemNav.vue
@@ -1,24 +1,37 @@
 <template>
-  <nav class="pokemon-nav d-flex">
-    <router-link class="pokemon-link d-flex align-items-center justify-content-center align-items-start"
-       style="background-color: grey; font-size: 25px;" :to="{name: 'pokedex-entry', params : { id: previousId } }">
-      <img src="../assets/images/arrow-left-circle.svg"
-           alt="left arrow"
-           width="32"
-           height="32"
-           class="nav-arrow">
-      <span class="ms-2 me-2 text-light">{{ fullId(previousId) }}</span>
-    </router-link>
-    <div class="p-1 bg-light"></div>
-    <router-link class="pokemon-link d-flex align-items-center justify-content-center align-items-center"
-       style="background-color: grey; font-size: 25px;" :to="{name: 'pokedex-entry', params : { id: nextId } }">
-      <span class="me-2 text-light" >{{ fullId(nextId) }}</span>
-      <img class="ms-2 nav-arrow"
-           src="../assets/images/arrow-right-circle.svg"
-           alt="right arrow"
-           width="32"
-           height="32">
-    </router-link>
+  <nav class="pokemon-nav d-flex flex-column bg-light">
+    <div>
+      <router-link class="btn btn-home"
+                   :to="{name: 'pokedex'}">
+        <img src="../assets/images/arrow-left-circle.svg"
+             alt="left arrow"
+             width="32"
+             height="32"
+             class="nav-arrow">
+        <span class="ms-2 me-2 fw-bold">HOME</span>
+      </router-link>
+    </div>
+    <div class="d-flex w-100">
+      <router-link class="pokemon-link d-flex align-items-center justify-content-center align-items-start"
+                   style="background-color: grey; font-size: 25px;" :to="{name: 'pokedex-entry', params : { id: previousId } }">
+        <img src="../assets/images/arrow-left-circle.svg"
+             alt="left arrow"
+             width="32"
+             height="32"
+             class="nav-arrow">
+        <span class="ms-2 me-2 text-light">{{ fullId(previousId) }}</span>
+      </router-link>
+      <div class="p-1 bg-light"></div>
+      <router-link class="pokemon-link d-flex align-items-center justify-content-center align-items-center"
+                   style="background-color: grey; font-size: 25px;" :to="{name: 'pokedex-entry', params : { id: nextId } }">
+        <span class="me-2 text-light" >{{ fullId(nextId) }}</span>
+        <img class="ms-2 nav-arrow"
+             src="../assets/images/arrow-right-circle.svg"
+             alt="right arrow"
+             width="32"
+             height="32">
+      </router-link>
+    </div>
   </nav>
 </template>
 
@@ -67,7 +80,16 @@ export default {
   bottom: 3px;
 }
 
-a:hover {
+a:hover, .btn-home:hover{
   background-color: #30a7d7 !important;
 }
+
+.btn-home {
+  background-color: grey;
+  padding: 1px 1px;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  color: white;
+}
+
 </style>
